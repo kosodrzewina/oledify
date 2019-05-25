c1f8bbdd1781b19b8246d1d91bab2fb4c8df7da9
Kosodrzewinatru <sienkiew@live.com>
2019-05-25 18:26:04 +0200

    image is displayed in imageView

--- app/src/main/java/com/kosodrzewinatru/oledify/MainActivity.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/MainActivity.kt
@@ -12,19 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // opening file picker
         uploadButton.setOnClickListener {
-            val intent = Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT)
+            val intent = Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT)
 
             startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
         }
     }
 
+    // result of quitting file picker
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 111 && resultCode == Activity.RESULT_OK) {
             val selectedFile = data?.data
-//            val pathS = selectedFile.toString()
+
+            // image showed via URI
+            imageView.setImageURI(selectedFile)
         }
     }
 }
