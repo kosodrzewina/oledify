b52c7c48440419bbb5914eec269b919b74a67147
Kosodrzewinatru <sienkiew@live.com>
2019-05-25 17:53:18 +0200

    added file picker

--- app/src/main/java/com/kosodrzewinatru/oledify/MainActivity.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.kosodrzewinatru.oledify
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        uploadButton.setOnClickListener {
+            val intent = Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT)
+
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 111 && resultCode == Activity.RESULT_OK) {
+            val selectedFile = data?.data
+//            val pathS = selectedFile.toString()
+        }
     }
 }
