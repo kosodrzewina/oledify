c74c6f56701fe93b2c4c14039c6d2ebb29899bee
Kosodrzewinatru <sienkiew@live.com>
2019-05-30 14:01:03 +0200

    got access to rgb from pixel

--- app/src/main/java/com/kosodrzewinatru/oledify/EditActivity.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/EditActivity.kt
@@ -1,9 +1,13 @@
 package com.kosodrzewinatru.oledify
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_edit.*
+import android.util.Log
 
 class EditActivity : AppCompatActivity() {
 
@@ -11,7 +15,30 @@ class EditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
-        val selectedFileEdit = intent.getStringExtra("selectedFileEdit")
-        imageEditView.setImageURI(Uri.parse(selectedFileEdit))
+        // set imageView src via URI from MainActivity
+        val selectedFileEdit = Uri.parse(intent.getStringExtra("selectedFileEdit"))
+        imageEditView.setImageURI(selectedFileEdit)
+
+        val bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedFileEdit)
+
+        process.setOnClickListener {
+            Editing.makeBlack(bitmap)
+        }
+    }
+}
+
+object Editing : AppCompatActivity() {
+
+    fun makeBlack(bitmap: Bitmap): Bitmap {
+        val pixels = IntArray(bitmap.width * bitmap.height)
+//        bitmap.getPixels(pixels, 0, 0, 0, 0, bitmap.width, bitmap.height)
+
+        val pixel = bitmap.getPixel(3, 7)
+
+        Log.d("red", Color.red(pixel).toString())
+        Log.d("green", Color.green(pixel).toString())
+        Log.d("blue", Color.blue(pixel).toString())
+        
+        return bitmap
     }
 }
