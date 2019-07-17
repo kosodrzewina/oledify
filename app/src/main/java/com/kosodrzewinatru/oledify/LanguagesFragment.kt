c464db3ba71018fdf93c36dd4cee7c89771a83ff
Kosodrzewinatru <sienkiew@live.com>
2019-07-17 16:27:29 +0200

    created fragment with list view (with placeholders, but still)

---
+++ app/src/main/java/com/kosodrzewinatru/oledify/LanguagesFragment.kt
@@ -0,0 +1,33 @@
+package com.kosodrzewinatru.oledify
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class LanguagesFragment: DialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.dialog_fragment_languages, container, false)
+
+        var menuItems = arrayOf<String>("Item_1", "Item_2", "Item_3")
+
+        val listView = view?.findViewById<ListView>(R.id.listLanguages)
+
+        var listViewAdapter = ArrayAdapter<String>(
+            activity,
+            android.R.layout.simple_list_item_1,
+            menuItems
+        )
+
+        listView?.adapter = listViewAdapter
+
+        return view
+    }
+}
