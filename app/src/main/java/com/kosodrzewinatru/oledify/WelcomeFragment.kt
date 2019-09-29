2fc0391e42ca9075d1117e96f7808b195d1561c4
Kosodrzewinatru <sienkiew@live.com>
2019-09-29 12:03:19 +0200

    added welcome fragment

---
+++ app/src/main/java/com/kosodrzewinatru/oledify/WelcomeFragment.kt
@@ -0,0 +1,17 @@
+package com.kosodrzewinatru.oledify
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WelcomeFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    }
+}
