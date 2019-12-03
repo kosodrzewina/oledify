d2da285ef0ea249d1b5e8169c06a7b13bfc94bd8
Kosodrzewinatru <sienkiew@live.com>
2019-12-03 13:56:15 +0100

    seekbarsStatus -> seekbarsState and added resState function

--- app/src/main/java/com/kosodrzewinatru/oledify/ImplementChanges.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/ImplementChanges.kt
@@ -1,22 +1,35 @@
 package com.kosodrzewinatru.oledify
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.preference.PreferenceManager
 import androidx.appcompat.widget.AppCompatSeekBar
 import com.kosodrzewinatru.oledify.activities.SettingsActivity
+import com.github.chrisbanes.photoview.PhotoView
 
-class ImplementChanges(context: Context, green: AppCompatSeekBar, blue: AppCompatSeekBar) {
+class ImplementChanges(context: Context,
+                       green: AppCompatSeekBar, blue: AppCompatSeekBar,
+                       photoView: PhotoView, bitmap: Bitmap, thumbnail: Bitmap) {
     init {
-        seekbarsStatus(context, green, blue)
+        seekbarsState(context, green, blue)
+        resState(context, photoView, bitmap, thumbnail)
     }
 
-    private fun seekbarsStatus(context: Context, green: AppCompatSeekBar, blue: AppCompatSeekBar) {
+    private fun seekbarsState(context: Context, green: AppCompatSeekBar, blue: AppCompatSeekBar) {
         if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(SettingsActivity.RGB_SLIDERS_SWITCH, false)) {
             green.isEnabled = true
             blue.isEnabled = true
         } else {
             green.isEnabled = false
             blue.isEnabled = false
+        }
+    }
+
+    private fun resState(context: Context, photoView: PhotoView, bitmap: Bitmap, thumbnail: Bitmap) {
+        if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(SettingsActivity.HIGH_RESOLUTION_SWITCH, false)) {
+            photoView.setImageBitmap(bitmap)
+        } else {
+            photoView.setImageBitmap(thumbnail)
         }
     }
 }
