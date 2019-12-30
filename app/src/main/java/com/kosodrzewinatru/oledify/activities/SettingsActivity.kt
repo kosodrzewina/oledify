66f4d1df96bcec44ad0a2216c28f0592fa7c06be
Kosodrzewinatru <sienkiew@live.com>
2019-12-30 20:09:07 +0100

    added localeToSharedPreferences function

--- app/src/main/java/com/kosodrzewinatru/oledify/activities/SettingsActivity.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/activities/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.kosodrzewinatru.oledify.activities
 
+import android.content.Context
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import com.kosodrzewinatru.oledify.fragments.SettingsFragment
+import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
     companion object {
@@ -15,5 +18,12 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
+    }
+
+    fun localeToSharedPreferences(context: Context, locale: Locale) {
+        val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+
+        editor.putString(LOCALE, locale.toString())
+        editor.apply()
     }
 }
