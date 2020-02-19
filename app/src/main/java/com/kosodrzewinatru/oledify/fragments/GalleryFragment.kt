8ceee148733d4f8a314192f6ec6f0d8dacb52833
Kosodrzewinatru <sienkiew@live.com>
2020-02-19 22:12:41 +0100

    created list of gallery items

--- app/src/main/java/com/kosodrzewinatru/oledify/fragments/GalleryFragment.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/fragments/GalleryFragment.kt
@@ -1,11 +1,14 @@
 package com.kosodrzewinatru.oledify.fragments
-
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kosodrzewinatru.oledify.GalleryItem
 import com.kosodrzewinatru.oledify.R
+import java.io.File
 
 class GalleryFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +16,17 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val imagesPath =
+            Environment.getExternalStorageDirectory().toString() + "/Pictures/Oledify"
+        val files = File(imagesPath).listFiles().toList()
+        val galleryItems = mutableListOf<GalleryItem>()
+
+        files.indices.forEach {
+            val currentBitmap = BitmapFactory.decodeFile(files[it].path)
+
+            galleryItems.add(GalleryItem(currentBitmap))
+        }
+
         return inflater.inflate(R.layout.fragment_gallery, container, false)
     }
 }
