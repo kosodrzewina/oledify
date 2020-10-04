d37658b1cd0b66ad343b75271e37301c59c39896
Kosodrzewinatru <sienkiew@live.com>
2020-10-04 16:46:13 +0200

    listening for theme change

--- app/src/main/java/com/kosodrzewinatru/oledify/activities/SettingsActivity.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/activities/SettingsActivity.kt
@@ -1,12 +1,16 @@
 package com.kosodrzewinatru.oledify.activities
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
-import androidx.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import com.kosodrzewinatru.oledify.ImplementStates
 import com.kosodrzewinatru.oledify.R
 import com.kosodrzewinatru.oledify.fragments.SettingsFragment
 import java.util.*
+
 
 class SettingsActivity : AppCompatActivity() {
     companion object {
@@ -15,7 +19,15 @@ class SettingsActivity : AppCompatActivity() {
         const val HIGH_RESOLUTION_SWITCH = "high_res_switch"
         const val LOCALE = "locale"
         const val THEME = "theme_drop_down"
+        private lateinit var preferences: SharedPreferences
     }
+
+    private var onSharedPreferenceChangeListener =
+        OnSharedPreferenceChangeListener { _, key ->
+            if (key == THEME) {
+                ImplementStates().themeState(this)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_SettingsScreen)
@@ -25,6 +37,18 @@ class SettingsActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
             .commit()
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferences.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferences.unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
     }
 
     fun localeToSharedPreferences(context: Context, locale: Locale) {
