aafcdf1a89486871f590338ad994185eecdafdac
Kosodrzewinatru <sienkiew@live.com>
2020-11-25 22:39:10 +0100

    don't need that import

--- app/src/main/java/com/kosodrzewinatru/oledify/fragments/GalleryFragment.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/fragments/GalleryFragment.kt
@@ -6,7 +6,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
-import android.support.v4.os.IResultReceiver
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
