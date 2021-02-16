87fa91b2c9a78f5797074aee0a037c87414d6034
Kosodrzewinatru <sienkiew@live.com>
2021-02-16 22:28:24 +0100

    added toast after setting wallpaper

--- app/src/main/java/com/kosodrzewinatru/oledify/fragments/ImagePreviewFragment.kt
+++ app/src/main/java/com/kosodrzewinatru/oledify/fragments/ImagePreviewFragment.kt
@@ -8,13 +8,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.snackbar.Snackbar
 import com.kosodrzewinatru.oledify.R
 import com.kosodrzewinatru.oledify.fragments.GalleryFragment
-import kotlinx.android.synthetic.main.activity_edit.*
-import kotlinx.android.synthetic.main.activity_edit.snackbar_container
-import kotlinx.android.synthetic.main.fragment_gallery.*
 import kotlinx.android.synthetic.main.fragment_image_preview.*
 import java.io.File
 
@@ -36,6 +33,7 @@ class ImagePreviewFragment(private val bitmap: Bitmap, private val path: String)
 
         open_button.setOnClickListener {
             WallpaperManager.getInstance(context).setBitmap(bitmap)
+            Toast.makeText(context, R.string.wallpaper_set, Toast.LENGTH_SHORT).show()
         }
 
         delete_button.setOnClickListener {
